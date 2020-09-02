fb4112bd69060bccf825c3a5353a402a7f6bee51
RCAPDART <rcapdart@gmail.com>
2020-09-02 20:29:40 +0300

    Update INHibernateHelper.cs
    Adding comments

--- Test_Work/Helpers/INHibernateHelper.cs
+++ Test_Work/Helpers/INHibernateHelper.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Test_Work.Helpers
 {
+    // Why you need interface for helper for NHibernate? How it can be replaced? It is already hard linked by name with NHibernate. At least it should be IOrmHelper or IDbHelper.
     public interface INHibernateHelper
     {
         ISessionFactory SessionFactory { get; }
